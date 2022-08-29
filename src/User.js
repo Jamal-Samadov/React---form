d82c8f9840f6f2a082b292d99ec8f6f18bb55c10
Jamaltscode <jamalts@code.edu.az>
2022-08-29 19:45:03 +0400

    Form task has done

---
+++ src/User.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+ class User extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+            <li>Ad:</li>
+            <li>Maaş:</li>
+            <li>Departament:</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default User
